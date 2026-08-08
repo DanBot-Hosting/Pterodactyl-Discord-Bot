ebe41bcca456ce828ef4253e4c801a7d0b195a8e
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2026-08-08 16:43:09 -0400

    chore: adding support for image for changelogs

--- src/commands/staff/changelog.js
+++ src/commands/staff/changelog.js
@@ -17,7 +17,9 @@ exports.run = async (client, message, args) => {
     // Checks if the user has the Developer Role.
     if (!message.member.roles.cache.find((r) => r.id === Config.DiscordBot.Roles.Developer)) return;
 
-    if (!args[1]) return message.reply("Please provide a message!");
+    const attachment = message.attachments.first();
+
+    if (!args[1] && !attachment) return message.reply("Please provide a message or attach an image!");
 
     const msg = message.content.split(" ").slice(2).join(" ");
 
@@ -30,7 +32,6 @@ exports.run = async (client, message, args) => {
                 url: `https://discord.com/users/${message.author.id}`,
             }
         )
-        .setDescription(msg)
         .setTimestamp()
         .setFooter(
             {
@@ -38,6 +39,11 @@ exports.run = async (client, message, args) => {
                 iconURL: client.user.displayAvatarURL({ format: "png", dynamic: true }),
             }
         );
+
+    if (msg) embed.setDescription(msg);
+    if (attachment && (attachment.contentType?.startsWith("image/") || /\.(png|jpe?g|gif|webp)$/i.test(attachment.name))) {
+        embed.setImage(attachment.url);
+    }
     
     const channel = await client.channels.cache.get(MiscConfigs.changelogs);
     await channel.send({content: `<@&${Config.DiscordBot.Roles.Changelogs}>`, embeds: [embed]});
